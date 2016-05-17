fa2000e208e24be0dfcfcfde6d4a2fdcfb6902d1
hayleethegamer <hayleethegamer@gmail.com>
2016-05-16 21:12:20 -0400

    Delete Chat_Commands.py

--- Chat_Commands.py
+++
@@ -1,9 +0,0 @@
-def chatCommands(message,client):
-	if message.content.startswith("Hello"):
-		msg = await client.send_message(message.channel, 'I will delete myself now...')
-		sendMessage(message,client,"Hello")
-	
-
-def sendMessage(message,client,sentMessage):
-	msg = await client.send_message(message.channel, sentMessage)
-	
